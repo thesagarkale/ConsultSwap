7ba2bb798273c7fd406a8c7f6c47a0bc5ea1f67b
Sagar Kale <sagarkale@Sagars-MacBook-Pro.local>
2021-05-30 18:31:10 +0100

    Redesign

---
+++ resources/js/components/ApplicationLogo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="logo" :style="cssVars">
+        Consultpeer.
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ApplicationLogo",
+    props: {
+        fontSize: {
+            type: [Number, String],
+            default: 1
+        },
+        color: {
+            type: String,
+            default: 'FFFFFF'
+        }
+    },
+    computed: {
+        cssVars() {
+            return {
+                'font-size': this.fontSize + 'rem',
+                'color': '#' + this.color
+            };
+        }
+    },
+}
+</script>
+
+<style scoped>
+.logo {
+    font-family: 'Suez One', serif;
+}
+</style>
