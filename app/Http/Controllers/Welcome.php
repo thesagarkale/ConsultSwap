fc0eff7311bae5595d8120c4d9e04f725b464fde
Sagar Kale <sagarkale@Sagars-MacBook-Pro.local>
2022-04-24 21:49:54 +0100

    WIP

---
+++ app/Http/Controllers/Welcome.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Transformers\CategoriesTransformer;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+
+class Welcome extends Controller
+{
+    public function index(Request $request): View
+    {
+        $categories = Category::inRandomOrder()->limit(20)->get();
+
+        return view('welcome', [
+            'categories' => $categories->all()
+        ]);
+    }
+}
